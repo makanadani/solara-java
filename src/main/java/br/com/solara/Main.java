d8ae8d1ea45ae8ef978b7347bc5769fe83969ee3
Marina Yumi Kanadani <makanadani@pm.me>
2024-11-21 03:26:47 -0300

    Gera Jar para o Railwind

--- src/main/java/br/com/solara/Main.java
+++ src/main/java/br/com/solara/Main.java
@@ -5,17 +5,11 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Configuração do servidor Jetty
         Server server = new Server(8080);
-
-        // Define o contexto do aplicativo web
         WebAppContext context = new WebAppContext();
         context.setContextPath("/");
         context.setWar("target/solara-java-0.0.1.war");
-
         server.setHandler(context);
-
-        // Inicia o servidor
         server.start();
         server.join();
     }
